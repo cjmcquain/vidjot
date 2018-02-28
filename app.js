c2367db5aca685dfdffc5c07aa7d7aaa301831d7
Charles McQuain <charlesmcquain@gmail.com>
2018-02-27 22:49:01 -0800

    Added Edit Idea functionality

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const exphbs = require ('express-handlebars');
+const methodOverride = require('method-override');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 
@@ -22,6 +23,8 @@ app.set('view engine', 'handlebars');
 app.use(bodyParser.urlencoded({ extended: false}));
 app.use(bodyParser.json());
 
+app.use(methodOverride('_method'));
+
 // Index Route
 app.get('/', (req, res) => {
   const title = 'Welcome';
@@ -35,8 +38,30 @@ app.get('/about', (req, res) => {
   res.render('about');
 });
 
+app.get('/ideas', (req, res) => {
+  Idea.find({})
+  .sort({date:'desc'})
+  .then(ideas => {
+    res.render('ideas/index', {
+      ideas
+    });
+  });
+});
+
 app.get('/ideas/add', (req, res) => {
   res.render('ideas/add');
+});
+
+app.get('/ideas/edit/:id', (req, res) => {
+  Idea.findOne({
+    _id: req.params.id
+  })
+  .then(idea => {
+    res.render('ideas/edit', {
+      idea:idea
+    });
+  });
+  
 });
 
 app.post('/ideas', (req, res) => {
@@ -55,8 +80,31 @@ app.post('/ideas', (req, res) => {
       details: req.body.details
     });
   } else {
-    res.send('Passed');
+    const newUser = {
+      title: req.body.title,
+      details: req.body.details
+    }
+    new Idea(newUser)
+      .save()
+      .then(idea => {
+        res.redirect('/ideas');
+      });
   }
+});
+
+app.put('/ideas/:id', (req, res) => {
+  Idea.findOne({
+    _id: req.params.id
+  })
+  .then(idea => {
+    idea.title = req.body.title;
+    idea.details = req.body.details;
+
+    idea.save()
+      .then(idea => {
+        res.redirect('/ideas');
+      })
+  });
 });
 
 const port = 5000;
