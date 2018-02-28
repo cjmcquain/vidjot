61356e08cfe6cbea4d1a3492e149b64a31666763
Charles McQuain <charlesmcquain@gmail.com>
2018-02-27 22:09:30 -0800

    Added Add Idea page

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const exphbs = require ('express-handlebars');
+const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 
 const app = express();
@@ -18,6 +19,9 @@ const Idea = mongoose.model('ideas');
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
 
+app.use(bodyParser.urlencoded({ extended: false}));
+app.use(bodyParser.json());
+
 // Index Route
 app.get('/', (req, res) => {
   const title = 'Welcome';
@@ -29,6 +33,30 @@ app.get('/', (req, res) => {
 // About Route
 app.get('/about', (req, res) => {
   res.render('about');
+});
+
+app.get('/ideas/add', (req, res) => {
+  res.render('ideas/add');
+});
+
+app.post('/ideas', (req, res) => {
+  let errors = [];
+
+  if(!req.body.title) {
+    errors.push({text: 'Please add a title'});
+  }
+  if(!req.body.details) {
+    errors.push({text: 'Please add some details'});
+  }
+  if(errors.length > 0) {
+    res.render('ideas/add', {
+      errors: errors,
+      title: req.body.title,
+      details: req.body.details
+    });
+  } else {
+    res.send('Passed');
+  }
 });
 
 const port = 5000;
