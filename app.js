a71995d8de1a29a68b5b7c293a610de89c786e62
Charles McQuain <charlesmcquain@gmail.com>
2018-03-06 22:53:00 -0800

    Finished Login functionality

--- app.js
+++ app.js
@@ -43,6 +43,10 @@ app.use(session({
   saveUnitialized: true
 }));
 
+// Passport Middleware
+app.use(passport.initialize());
+app.use(passport.session());
+
 app.use(flash());
 
 // Global Variables
@@ -50,6 +54,7 @@ app.use(function(req, res, next) {
   res.locals.success_msg = req.flash('success_msg');
   res.locals.error_msg = req.flash('error_msg');
   res.locals.error = req.flash('error');
+  res.locals.user = req.user || null;
   next();
 });
 
--- config/passport.js
+++ config/passport.js
@@ -10,6 +10,33 @@ module.exports = function(passport) {
     usernameField: 'email'
   },
   (email, password, done) => {
-    console.log(email);
+    // Match User
+    User.findOne({
+      email:email
+    }).then(user => {
+      if(!user) {
+        return done(null, false, {message: 'No User Found'});
+      }
+      // Match Password
+      bcrypt.compare(password, user.password, (err, isMatch) => {
+        if(err) throw err;
+        if(isMatch) {
+          return done(null, user);
+        } else {
+          return done(null, false, {message: 'Password Incorrect'});
+        }
+      })
+
+    })
   }));
+
+  passport.serializeUser(function(user, done){
+    done(null, user.id);
+  });
+
+  passport.deserializeUser(function(err, user) {
+    User.findById(id, function(err, user) {
+      done(err, user);
+    });
+  });
 }
