a01c1b9344f3b1e63aa14840a621de8e8a644716
Charles McQuain <charlesmcquain@gmail.com>
2018-03-06 23:16:19 -0800

    Added Logout functionality & Configured User Access Control

--- config/passport.js
+++ config/passport.js
@@ -30,11 +30,11 @@ module.exports = function(passport) {
     })
   }));
 
-  passport.serializeUser(function(user, done){
+  passport.serializeUser(function(user, done) {
     done(null, user.id);
   });
-
-  passport.deserializeUser(function(err, user) {
+  
+  passport.deserializeUser(function(id, done) {
     User.findById(id, function(err, user) {
       done(err, user);
     });
--- routes/ideas.js
+++ routes/ideas.js
@@ -1,13 +1,15 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const router = express.Router();
+const {ensureAuthenticated} = require('../helpers/auth');
+
 
 // Load Idea Model
 require('../models/Idea');
 const Idea = mongoose.model('ideas');
 
-router.get('/', (req, res) => {
-  Idea.find({})
+router.get('/', ensureAuthenticated, (req, res) => {
+  Idea.find({user: req.user.id})
   .sort({date:'desc'})
   .then(ideas => {
     res.render('ideas/index', {
@@ -16,23 +18,29 @@ router.get('/', (req, res) => {
   });
 });
 
-router.get('/add', (req, res) => {
+router.get('/add', ensureAuthenticated, (req, res) => {
   res.render('ideas/add');
 });
 
-router.get('/edit/:id', (req, res) => {
+router.get('/edit/:id', ensureAuthenticated, (req, res) => {
   Idea.findOne({
     _id: req.params.id
   })
   .then(idea => {
-    res.render('ideas/edit', {
-      idea:idea
-    });
+    if(idea.user != req.user.id) {
+      req.flash('error_msg', 'Not Authorized');
+      res.redirect('/ideas');
+    } else {
+      res.render('ideas/edit', {
+        idea:idea
+      });
+    }
+    
   });
   
 });
 
-router.post('/', (req, res) => {
+router.post('/', ensureAuthenticated, (req, res) => {
   let errors = [];
 
   if(!req.body.title) {
@@ -50,7 +58,8 @@ router.post('/', (req, res) => {
   } else {
     const newUser = {
       title: req.body.title,
-      details: req.body.details
+      details: req.body.details,
+      user: req.user.id
     }
     new Idea(newUser)
       .save()
@@ -61,7 +70,7 @@ router.post('/', (req, res) => {
   }
 });
 
-router.put('/:id', (req, res) => {
+router.put('/:id', ensureAuthenticated, (req, res) => {
   Idea.findOne({
     _id: req.params.id
   })
@@ -77,7 +86,7 @@ router.put('/:id', (req, res) => {
   });
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', ensureAuthenticated, (req, res) => {
   Idea.remove({_id: req.params.id})
     .then(() => {
       req.flash('success_msg', 'Video Idea Successfully Removed');
